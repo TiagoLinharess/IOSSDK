9c9316bc32587e71e7321da136e03b57b244687e
TiagoLinharess <tiagolinharessouza@gmail.com>
2023-07-12 18:12:37 -0300

    Implements UIMVVMBaseViewController tests

---
+++ SDKCoreTests/Source/Mocks/UIKit/MVVM/EmptyViewModelMock.swift
@@ -0,0 +1,10 @@
+//
+//  EmptyViewModelMock.swift
+//  SDKCoreTests
+//
+//  Created by Tiago Linhares on 12/07/23.
+//
+
+import Foundation
+
+final class EmptyViewModelMock {}
---
+++ SDKCoreTests/Source/Tests/UIKit/MVVM/ViewController/UIMVVMBaseViewControllerTests.swift
@@ -0,0 +1,30 @@
+//
+//  UIMVVMBaseViewControllerTests.swift
+//  SDKCoreTests
+//
+//  Created by Tiago Linhares on 12/07/23.
+//
+
+import SnapshotTesting
+import XCTest
+@testable import SDKCore
+
+final class UIMVVMBaseViewControllerTests: XCTestCase {
+    
+    var sut: UIMVVMBaseViewController<EmptyViewModelMock>!
+
+    override func setUpWithError() throws {
+        sut = .init(viewModel: EmptyViewModelMock())
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func test_snapshot() throws {
+        sut.title = "UIMVVMBaseViewController"
+        sut.view.backgroundColor = .white
+        let vc = UINavigationController(rootViewController: sut)
+        assertSnapshot(matching: vc, as: .image)
+    }
+}
