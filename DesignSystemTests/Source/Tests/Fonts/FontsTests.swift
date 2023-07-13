4337ce648c2cfb7b8cb6caedcd41d38db7412176
TiagoLinharess <tiagolinharessouza@gmail.com>
2023-07-13 14:31:15 -0300

    Implements fonts tests

---
+++ DesignSystemTests/Source/Tests/Fonts/FontsTests.swift
@@ -0,0 +1,25 @@
+//
+//  FontsTests.swift
+//  DesignSystemTests
+//
+//  Created by Tiago Linhares on 13/07/23.
+//
+
+import SnapshotTesting
+import XCTest
+@testable import DesignSystem
+
+final class FontsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        DesignSystemConfiguration.loadFonts()
+    }
+
+    func test_snapshot() throws {
+        let label = UILabel()
+        label.text = "Hello test"
+        label.font = .title1(.roboto, .bold)
+        
+        assertSnapshot(matching: label, as: .image)
+    }
+}
